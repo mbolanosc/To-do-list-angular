c36d226f2ca273056f06062b5599ec2c447e6ed3
meluk <kmelisu@hotmail.com>
2016-02-06 13:56:30 -0600

    inicios de los Detalles
    Agregué botón de lista, cree una función de borrar detalle, que se bebe
    arreglar….

--- js/controllers/controller2.js
+++ js/controllers/controller2.js
@@ -1,10 +1,24 @@
-angular.module('ControllersDetalle', [])
+angular.module('ControllersDetalle', ['LocalStorageModule'])
 
-.controller('detalleCtrl', function ($scope , $routeParams, $rootScope) {
-   //$scope.arregloDeTareas = [{textoTarea: 'Comprar comida para Borona', textoDescripcion:'Ir al super a comprar comida para chihuahuas.', elementSeleccionado:false}];
+.controller('detalleCtrl', function ($scope , $routeParams, $rootScope, localStorageService) {
+
+   
     $rootScope.whichItem = $routeParams.itemId;
     console.log($rootScope.whichItem);
-    /*
-    Vacio Por ahora, se lo paso a Meli
-    */
-});
+    var key = $rootScope.whichItem;
+
+   /*var removeItem = function(key) {
+   return localStorageService.remove(key);
+  }*/
+	
+	$rootScope.eliminarDetalles = function(){
+		 /*localStorage.removeItem($rootScope.whichItem);
+		localStorageService.set("tareas",$scope.arregloDeTareas);*/
+		
+		$rootScope.arregloDeTareas.slice($rootScope.whichItem,1);
+		//$rootScope.whichItem = null; // prueba para saber su trabaja sobre le array
+		
+		console.log(localStorageService.get('tareas'));
+	};
+    
+})
